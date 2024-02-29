04f911fd793332cd34c0f0c03b9cb43a64dd67ae
Eduardo C. Resplandes <resplandes930@gmail.com>
2024-02-28 22:12:46 -0300

    Estilização de Relatório

--- resources/views/reports/stock.blade.php
+++ resources/views/reports/stock.blade.php
@@ -6,24 +6,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Relatório</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <style>
+        /* Estilo adicional para centralizar a tabela */
+        .table-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+    </style>
+
 </head>
 
 
 <body>
-    <div style="text-align: right;">
-    </div>
     <div class="container">
 
         <div class="row">
+
             <div style="text-align: right">
                 <p style="font-weight: 300;">Data de Emissão: {{ $dateTime }}</p>
             </div>
+
         </div>
 
         <div class="row">
 
             <div style="text-align: center;" class="col-12">
-                <img src="../../../resources/img/logo.png">
+                <img src="../../../public/img/logo.png">
                 <h3>RELATÓRIO DE ESTOQUE | {{ !empty($companie) ? $companie : 'GERAL' }} </h3>
                 <br>
             </div>
@@ -33,7 +44,7 @@
         <div class="row">
             <div class="col-12">
                 <table style="border: 2px solid black; text-align: center; border-collapse: collapse;" class="table">
-                    <thead>
+                    <thead style="background-color: #f0f0f2;">
                         <tr>
                             <th style=" border: 1px solid black; padding: 10px" scope="col-1">ID</th>
                             <th style=" border: 1px solid black; padding: 10px;" scope="col-2">MATERIAL</th>
@@ -45,7 +56,7 @@
                     </thead>
                     <tbody>
                         @foreach($informations as $information)
-                        <tr>
+                        <tr style="<?php if ($information->amount == 0) echo 'color: red;'; ?>">
                             <td style=" border: 1px solid black; padding: 10px">{{ $information->id_stock }}</td>
                             <td style=" border: 1px solid black; padding: 10px">{{ $information->material_name }}</td>
                             <td style=" border: 1px solid black; padding: 10px">{{ $information->description }}</td>
